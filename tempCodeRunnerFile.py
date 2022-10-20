6a52391d2ea6b9e5bbf15df23919b6773b65b2b7
Krishn1412 <76171905+Krishn1412@users.noreply.github.com>
2022-10-20 13:04:11 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
- # no_of_property=int(input("Enter number of property: "))
-    # for i in range(no_of_property):
-    #     list_of_prevtrans=[]
-    #     id=int(input("Enter ID of property: "))
-    #     name=input("Enter property name: ")
-    #     curr_owner=input("Enter the name of curr owner: ")
-    #     prop=property(curr_owner,id,name,list_of_prevtrans)
-    #     arr_of_prop.append(prop)
